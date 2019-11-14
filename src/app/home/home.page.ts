346b43bc4df9c85e16b576d210dd9efbb27475ec
Anas-Emmanuel FAOUR <anas-emmanuel.faour@orange.fr>
2019-11-14 09:12:01 +0100

    Play with animal sound

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,98 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  animals = [
+    {
+      'title': 'Vache',
+      'image': 'img/animals/cow-icon.png',
+      'desc': 'Meugle',
+      'file': '/sounds/cow.mp3',
+      'playing': false
+    },
+    {
+      'title': 'Dauphin',
+      'image': 'img/animals/dolphin-icon.png',
+      'desc': 'Siffle',
+      'file': '/sounds/dolphin.mp3',
+      'playing': false
+    },
+    {
+      'title': 'Grenouille',
+      'image': 'img/animals/frog-icon.png',
+      'desc': 'Coasse',
+      'file': '/sounds/frog.mp3',
+      'playing': false
+    },
+    {
+      'title': 'Oiseau',
+      'image': 'img/animals/bird-icon.png',
+      'desc': 'Chante',
+      'file': '/sounds/bird.mp3',
+      'playing': false
+    },
+    {
+      'title': 'Cochon',
+      'image': 'img/animals/pig-icon.png',
+      'desc': 'Grogne',
+      'file': '/sounds/pig.mp3',
+      'playing': false
+    },
+    {
+      'title': 'Chien',
+      'image': 'img/animals/puppy-icon.png',
+      'desc': 'Aboie',
+      'file': '/sounds/dog.mp3',
+      'playing': false
+    },
+    {
+      'title': 'Chat',
+      'image': 'img/animals/black-cat-icon.png',
+      'desc': 'Miaule',
+      'file': '/sounds/cat.mp3',
+      'playing': false
+    },
+    {
+      'title': 'Cheval',
+      'image': 'img/animals/horse-icon.png',
+      'desc': 'Hennit',
+      'file': '/sounds/horse.wav',
+      'playing': false
+    },
+    {
+      'title': 'Ane',
+      'image': 'img/animals/donkey-icon.png',
+      'desc': 'Brait',
+      'file': '/sounds/donkey.wav',
+      'playing': false
+    }
+  ];
 
-  constructor() {}
+  constructor() { }
 
+  private media = null;
+  private currentAnimalIndex: number = null;
+
+  playSound() {
+    this.currentAnimalIndex = Math.floor(Math.random() * this.animals.length);
+    let animal = this.animals[this.currentAnimalIndex];
+
+
+    console.log(animal);
+
+    this.media = new Audio('/assets' + animal.file);
+    this.media.load();
+    this.media.play();
+  }
+  guessAnimal(pos) {
+    let message = "";
+    if (this.currentAnimalIndex == null) {
+      message = "Il faut d'abord jouer avant de choisir un animal";
+    } else if (this.currentAnimalIndex == pos) {
+      let animal = this.animals[pos];
+      message = `Bien joué c'est bien le jouer ${animal.title} qui ${animal.desc}`
+    } else {
+      message = "ce n'est pas ça, essaie encore";
+    }
+    console.log(message)
+  }
 }
